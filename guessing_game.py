0cbef0bda42b128f2c43f699d8aa5f7aa8688f78
Keith Douglas <keith.douglas@codeclan.com>
2021-04-19 12:54:26 +0100

    added gussing game code

---
+++ guessing_game.py
@@ -0,0 +1,13 @@
+correct_guess = 4
+
+guess = input("Guess a number:")
+
+while True:
+
+  if int(guess) == correct_guess:
+    print("Correct!")
+    break
+  else:
+    print("Try again")
+  
+  guess = input("Guess a number:")
